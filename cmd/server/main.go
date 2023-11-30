95bac07a471e848a2b8e554a045dbc07639f522a
Godnelson <luizgatohd@gmail.com>
2023-11-29 23:04:16 -0300

    database connection implemented

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"e-commerce-golang/pkg/db"
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
+	"log"
 	"os"
 )
 
 func main() {
 	_ = godotenv.Load()
 	app := fiber.New()
+
+	connection := db.New()
+
+	err := connection.Client.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.JSON(&fiber.Map{
--- pkg/db/connection.go
+++ pkg/db/connection.go
@@ -3,16 +3,19 @@ package db
 import (
 	"fmt"
 	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 	"log"
 	"os"
 )
 
-type Connection struct{}
+type Connection struct {
+	Client *sqlx.DB
+}
 
-func (d *Connection) New() *sqlx.DB {
+func New() Connection {
 	db, err := sqlx.Connect("postgres", fmt.Sprintf("user=root password=%s dbname=%s sslmode=disable", os.Getenv("DATABASE_PASSWORD"), os.Getenv("DATABASE_NAME")))
 	if err != nil {
 		log.Fatal(err)
 	}
-	return db
+	return Connection{db}
 }
