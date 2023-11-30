6a71a33c5a602cd16beecb211d20658f915bacba
Godnelson <luizgatohd@gmail.com>
2023-11-29 23:05:13 -0300

    start repository implementations

--- pkg/ecommerce/category/db.go
+++ pkg/ecommerce/category/db.go
@@ -1,1 +1,19 @@
 package category
+
+import (
+	"github.com/jmoiron/sqlx"
+	"log"
+)
+
+type Query struct {
+	db *sqlx.DB
+}
+
+func (q *Query) GetAll() []Category {
+	var categories []Category
+	err := q.db.Select(&categories, "SELECT * FROM category")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return categories
+}
