13eb201bd1796b2153a6e1b7a6ab3bebc49f6ed2
Godnelson <luizgatohd@gmail.com>
2023-11-29 21:48:41 -0300

    basic routing added

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,15 @@
-package server
+package main
+
+import "github.com/gofiber/fiber/v2"
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(&fiber.Map{
+			"Hello": "World",
+		})
+	})
+
+	_ = app.Listen(":8000")
+}
